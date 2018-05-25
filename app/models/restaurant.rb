575bb3bc3f6057695c0d4de117f2db79b5877fe5
Reyes Montemayor <sjpete1234@hotmail.com>
2018-05-25 09:57:39 +0900

    adding border and editing index

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,9 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"]}
+
+  def review_average
+    @ratings = reviews.pluck(:rating)
+
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,5 @@ class Review < ApplicationRecord
   validates :rating, inclusion: { in: [ 0, 1, 2, 3, 4, 5]}
   validates :rating, numericality: true
 
+
 end
